e6fbafae6759263827b3b94e4375330ef459dd92
ctsai <ctsai@netdb.cs.nthu.edu.tw>
2016-09-09 13:09:06 +0800

    Remove txt file output, Fix some parsing bug

--- data-crawler/metadata-crawler.py
+++ data-crawler/metadata-crawler.py
@@ -1,6 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 09 10:16:41 2016
+
+@author: SISQUAKE
+"""
+
 import requests
 import json
 from bs4 import BeautifulSoup
+from optparse import OptionParser
+
+parser = OptionParser();
+parser.add_option("-o",dest = "OUTPUT_DIRECTORY");
+(options, args) = parser.parse_args();
+
 ids = ['620', u'282070', '264200', '427520', '341800', '400', '424280', u'292030', '374570', '238460', '342560', '433950', '10', u'292030', '413150', '214560', '351640', '420110', '411960', '219150', '250900', '219740', '250320', '220200', '227300', '247080', '213670', '413420', '274190', u'219740', '107100', '102600', '312990', '550', '400910', '231160', '413410', '324160', '206190', '293780', '316720', '205100', '212680', '370360', '407900', u'34330', '265610', '450390', '18500', '32150', '206440', '207610', '220', '105600', '391540', '48700', '367580', '227000', '322170', '70400', '49520', '221640', '319630', '35720', u'8930', '368340', u'237930', '284770', '360740', '13230', '204360', '218410', '13240', '8930', u'204360', '339350', '35140', '251370', '428550', '55230', '242680', u'391540', '322330', '266010', '261570', '236930', u'113200', '373770', '288160', u'227300', '431730', '9420', '238320', '236090', '279800', '224820', '280220', u'24960', '3590', '113200']
 
 itr = 0  
@@ -8,8 +21,7 @@ attrs = ['App ID','App Type','Name','Developer','Publisher','Release Date']
 
 
 for appid in ids :
-    out = ""
-
+    
     response = requests.get("https://steamdb.info/app/"+appid+"/")
     
     if response.status_code == 200:
@@ -21,7 +33,8 @@ for appid in ids :
         table = BeautifulSoup(str(meta))
         rows = table.find_all('td')
         if(rows[3].string!='Downloadable Content'):
-            print 'Parse APP:' + appid + " no " + str(itr)
+            
+            print 'Parse APP:' + appid + " No " + str(itr)
             for a in attrs:
                 for j in range(len(rows)/2):
                     attr = str(rows[2*j]).replace(">","#").replace("<","#").split("#")[2]
@@ -33,45 +46,57 @@ for appid in ids :
                             val = str(rows[2*j+1]).replace(">","#").replace("<","#").split("#")[2]
                         
                         app_dict[attr] = val;
-                        out = out + attr + "|" + val  + "\n"
+
                         break
             
             info = BeautifulSoup(str(soup.find_all(id = 'info')));
             rows = info.find_all('td')
             
+            app_dict['metacritic_score'] = -1;
+            app_dict['dlc'] = 0;
+            
             # Genres
+            l = len(rows);            
+            for i in range(0,l,2):
+                if rows[i].string == 'Genres' :
+                    app_dict['Genres'] = rows[i+1].string;
+                    
+                elif rows[i].string == 'metacritic_score' :
+                    app_dict['metacritic_score'] = rows[i+1].string;
+                    dlc = BeautifulSoup(str(soup.find_all(href = '/app/'+appid+'/dlc/')));
+                    dlc_rows = dlc.find_all('span')
+                    if len(dlc_rows) > 0:
+                        # dlc count
+                        app_dict['dlc'] = int(dlc_rows[1].string);
+                        
+            '''
             app_dict[rows[6].string] = rows[7].string;
-            out = out + rows[6].string + "|" + rows[7].string + "\n"
+            
             if(rows[18].string == 'metacritic_score'):
                 # metacritic_score
                 app_dict[rows[18].string] = rows[19].string;
-                out = out + rows[18].string + "|" + rows[19].string + "\n"
                                 
                 dlc = BeautifulSoup(str(soup.find_all(href = '/app/'+appid+'/dlc/')));
                 rows = dlc.find_all('span')
                 if len(rows) > 0:
                     # dlc count
                     app_dict['dlc'] = rows[1].string;
-                    out = out + "dlc|" + rows[1].string+ "\n"
+            
                 else:
                      app_dict['dlc'] = 0;
             else:
                 app_dict['metacritic_score'] = -1;
                 app_dict['dlc'] = 0;
-            
-            f = open('app_meta_'+appid+'.txt','w')
-            f.write(''.join(out));
-            f.close();
-            
-             
-            f = open('app_meta_'+appid+'.json','w')
+            '''           
+
+            f = open(options.OUTPUT_DIRECTORY+'app_meta_'+appid+'.json','w');
             f.write(''.join(json.dumps(app_dict, ensure_ascii=False) ));
             f.close();
             
             itr = itr + 1;
             
         else:
-            print "Error on no :"+str(itr)
+            print "Error on No :"+str(itr)
             itr = itr + 1;
     
     
--- data-preprocessor/preprocessor.py
+++ data-preprocessor/preprocessor.py
@@ -6,6 +6,7 @@ Created on Thu Sep 08 16:16:41 2016
 """
 from optparse import OptionParser
 import os
+from Game import Game
 
 def listFilePath(path):
     file_path = [];
@@ -20,6 +21,12 @@ def listFilePath(path):
     return {'file_path':file_path,'file_name':file_name}
 
 #def main():
+
+METADATA_DIRECTORY = '../../new_meta';
+PRICE_DATA_DIRECTORY='../../price_data';
+TRAINING_OUTPUT_DIRECTORY = '../../traing_data';
+
+TARGET_COUNTRY = 'us';
     
 parser = OptionParser();
 parser.add_option("-m",dest = "METADATA_DIRECTORY");
@@ -27,9 +34,12 @@ parser.add_option("-p",dest="PRICE_DATA_DIRECTORY");
 parser.add_option("-t",dest="TRAINING_OUTPUT_DIRECTORY");
 (options, args) = parser.parse_args();
 
-#print options.METADATA_DIRECTORY;
-#print parser.PRICE_DATA_DIRECTORY;
-#print parser.OUTPUT_TRAINING;
+if options.METADATA_DIRECTORY != None:
+    METADATA_DIRECTORY = options.METADATA_DIRECTORY;
+if options.PRICE_DATA_DIRECTORY != None:
+    PRICE_DATA_DIRECTORY = options.PRICE_DATA_DIRECTORY;
+if options.TRAINING_OUTPUT_DIRECTORY != None:
+    TRAINING_OUTPUT_DIRECTORY = options.TRAINING_OUTPUT_DIRECTORY;
 
 # Store for Binary feature needs
  
@@ -39,12 +49,16 @@ genres_set = {};
 
 # Store ID to Game Object
 
-list_meta = listFilePath('../new_meta');
+game_set = {};
+
+list_meta = listFilePath(METADATA_DIRECTORY);
 
 for meta_path in list_meta['file_path']:
-    
+    g = Game(meta_path,TARGET_COUNTRY);
+    print g.id
+    game_set[g.id] = g;
         
-    
+
 #if __name__ == "__main__":
 #    main();
 
