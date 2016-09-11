757812e63c42f38ad065c31744427c5902a5d322
ctsai <ctsai@netdb.cs.nthu.edu.tw>
2016-09-11 13:58:33 +0800

    Move some Utils outside of the main script.

---
+++ data-preprocessor/listFile.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 11 10:23:38 2016
+
+@author: SISQUAKE
+"""
+import os
+def listFilePath(path):
+    File = [];
+    Dir = [];
+
+    for (dirpath, dirnames, filenames) in os.walk(path):
+        for name in filenames:
+            tmp = os.path.join(dirpath, name);
+            File.append({'path':tmp,'name':name});
+        for name in dirnames:
+            tmp = os.path.join(dirpath, name);
+            Dir.append({'path':tmp,'name':name});
+        break
+
+    return {'file' : File , 'dir' : Dir};
