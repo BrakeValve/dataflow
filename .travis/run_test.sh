aa3253d22d4113fc9a05eba8a621c7db7db46e2a
ctsai <ctsai@netdb.cs.nthu.edu.tw>
2016-09-12 11:40:13 +0800

    New testing shell script

--- .travis/run_test.sh
+++ .travis/run_test.sh
@@ -3,4 +3,10 @@ cd .travis
 tar xf test_data.tar.gz
 cd ..
 python data-preprocessor/preprocessor.py -m .travis/test_meta/ -p .travis/test_price_data/ -t .travis/test_trainging
+code=$?
+if [ $code ne 1 ]
+then
+	echo "Sample training data preprocessing : FAILED"
+	exit $code
+fi
 echo "Sample training data preprocessing : PASSED"
